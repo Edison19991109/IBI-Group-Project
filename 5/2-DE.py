411dcae9e6dc926cdb739751b2a7c2d30fdc5866
Valerya <13588262185@163.com>
2019-04-21 16:40:31 +0800

    added axis

--- 5/2-DE.py
+++ 5/2-DE.py
@@ -8,15 +8,19 @@ import matplotlib.pyplot as plt
 from numpy import log
 
 P = {'A':(6.8,5500),'B':(8.7,550),'C':(4.8,7890)}
-plt.figure(figsize=(10,12))
+fig = plt.figure(figsize=(10,12))
+ax = fig.add_subplot(111)
 for k in P:
     pH=P[k][0]
     Mw=10-log(P[k][1]/1000)
-    plt.plot(pH,Mw,'o',label=k+":pH=%d,mass=%d"%(P[k][0],P[k][1]))
+    ax.plot(pH,Mw,'o',label=k+":pH=%d,mass=%d"%(P[k][0],P[k][1]))
 plt.title('2-DE')
-plt.xticks([])
-plt.yticks([])
-plt.xlim(4,10)
-plt.ylim(10-log(10),)
-plt.legend(loc='lower right')
+ax.set_xlabel('pH')
+ax.set_ylabel('Mw(Da)')
+ax.set_xticks(range(4,10))
+ax.set_yticks([10-log(10),10-log(5),10-log(2),10-log(1.5),10-log(1),10-log(0.5),10-log(0.2),10-log(0.1)])
+ax.set_yticklabels([10000,5000,2000,1500,1000,500,200,100])
+ax.set_xlim(4,10)
+ax.set_ylim(10-log(10),)
+ax.legend(loc='lower right')
 plt.show()
