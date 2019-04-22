d9ca6aa5594982411056a7abbc3e586e720b2f7f
Valerya <13588262185@163.com>
2019-04-22 09:46:26 +0800

    added annotation

--- 5/5_incomplete.py
+++ 5/5_incomplete.py
@@ -6,12 +6,12 @@ Created on Sun Apr 21 20:20:20 2019
 """
 
 #现实中同种氨基酸R基在一条多肽链不同位置pKa会发生变化，此程序无法模拟。当同种氨基酸在一条多肽链中出现次数过多，可能无法计算pI
-L = {'A':'PDAKVL',
-     'B':'EPHINAQIM',
-     'C':'LGSRQKHSLPDLPYDYGAL',
-     'D':'MLSRAVCGTSRQLAPVLAY',
+L = {'A':'D',
+     'B':'PAKVL',
+     'C':'EPHINAQIM',
+     'D':'LGSRQKHSLPDLPYDYGAL',
      'E':'INHSIFWTNLSPNGGGEPKGELLEAIKRDFGSFDKFKE',
-     'F':'QLHHSKADAAYVNNLNVTVEAYQEALAKGDVTAQIALQPALKFNGGGV'}
+     'F':'QLHHSKADAAYVNNLNVTVEAYQEALAKGDVTAQIALQPALKFNGGGV'} #可以换成input输入多肽
 
 A ={'G':(75,2.34,9.60),'A':(89,2.34,9.69),'P':(115,1.99,10.96),'V':(117,2.32,9.62),
     'L':(131,2.36,9.60),'I':(131,2.36,9.68),'M':(149,2.28,9.21),'F':(165,1.83,9.13),
@@ -20,14 +20,14 @@ A ={'G':(75,2.34,9.60),'A':(89,2.34,9.69),'P':(115,1.99,10.96),'V':(117,2.32,9.6
     'H':(155,1.82,9.17,6.00,1),'R':(174,2.17,9.04,12.48,1),'D':(133,1.88,9.60,3.65,0),'E':(147,2.19,9.67,4.25,0)}
 #Nelson, David L.(David Lee),1942-author.  Lehninger principles of biochemistry /. 7th ed. 
 #New York : W. H. Freeman and Company ; Houndmills, Basingstoke : Macmillan Higher Education, c2017.
-P = {}
+P = {} #这个字典用来存多肽信息
 
-def count(L,a,b):
+def count(L,a,b): #数带正/负电氨基酸残基
     x = 0
     for i in L:
         if b == 1:
             if i<a:
-                x += 1
+                x += 1    
         else:
             if i>a:
                 x += 1
@@ -35,25 +35,25 @@ def count(L,a,b):
     
 
 for k in L:
-    Mw = 0
-    pI = 0
-    pKa = [A[L[k][0]][1]]
-    pKb = [A[L[k][len(L[k])-1]][2]]
+    Mw = 0 #分子量
+    pI = 0 #等电点
+    pKa = [A[L[k][0]][1]] #C端羧基 3HN+/COOH <=A[k][1]=> 3HN+/COO-
+    pKb = [A[L[k][len(L[k])-1]][2]] #N端氨基 3HN+/COO- <=A[k][2]=> 2HN/COO-
     for i in range(len(L[k])):
-        Mw += A[L[k][i]][0]
-        if len(A[L[k][i]]) == 5:
-            if A[L[k][i]][4] == 0:
-                pKa.append(A[L[k][i]][3])
-            else:
-                pKb.append(A[L[k][i]][3])
+        Mw += A[L[k][i]][0] #加上当前氨基酸分子量
+        if len(A[L[k][i]]) == 5: #若当前氨基酸R基可电离
+            if A[L[k][i]][4] == 0: #R <=A[k][3]=> R-
+                pKa.append(A[L[k][i]][3]) #pKa：pH升高时带负电->不带电
+            else: #R+ <=A[k][3]=> R
+                pKb.append(A[L[k][i]][3]) #pKa：pH升高时不带电->带正电
     pKa.sort(reverse=True)
     pKb.sort()
-    print(pKa,'\n',pKb)
-    for i in range(min([len(pKa),len(pKb)])-1):
+    ###print(pKa,'\n',pKb)
+    for i in range(min([len(pKa),len(pKb)])):
         pH = (pKa[i]+pKb[i])/2
-        if count(pKa,pH,0) == count(pKb,pH,1):
+        if count(pKa,pH,0) == count(pKb,pH,1): #还要再讨论：某些多肽链中同种氨基酸多次出现，同时电离导致pH>某点时多肽带负电，<某点时带负电的情况
             pI = pH
-            print(pKa[i],pKb[i])
+            ###print(pKa[i],pKb[i])
             break
     P[k] = (pI,Mw-18*(len(L[k])-1))
 
@@ -64,15 +64,15 @@ fig = plt.figure(figsize=(10,12))
 ax = fig.add_subplot(111)
 for k in P:
     pI=P[k][0]
-    Mw=10-log(P[k][1]/1000)
-    ax.plot(pI,Mw,'o',label=k+":pI=%d,Mw=%d"%(P[k][0],P[k][1]))
+    Mw=10-log(P[k][1]/1000) #logMw与距离成线性关系，Mw越大距离越短
+    ax.plot(pI,Mw,'o',label=k+':pI='+str(P[k][0])+'Mw='+str(P[k][1]))
 plt.title('2-DE')
 ax.set_xlabel('pH')
 ax.set_ylabel('Mw(Da)')
-ax.set_xticks(range(4,10))
+ax.set_xticks(range(3,112))
 ax.set_yticks([10-log(10),10-log(5),10-log(2),10-log(1.5),10-log(1),10-log(0.5),10-log(0.2),10-log(0.1)])
 ax.set_yticklabels([10000,5000,2000,1500,1000,500,200,100])
-ax.set_xlim(4,10)
+ax.set_xlim(3,11)
 ax.set_ylim(10-log(10),)
 ax.legend(loc='lower right')
 plt.show()
